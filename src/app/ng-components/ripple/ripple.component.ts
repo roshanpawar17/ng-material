ec1d732b178f8dc7c7ddb912a40902c1db4f4915
roshanpawar17 <roshanpawar119@gmail.com>
2024-05-12 02:20:40 +0530

    manual ripple

--- src/app/ng-components/ripple/ripple.component.ts
+++ src/app/ng-components/ripple/ripple.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { MatRipple, RippleGlobalOptions } from '@angular/material/core';
+import { MAT_RIPPLE_GLOBAL_OPTIONS } from '@angular/material/core';
 
 @Component({
   selector: 'ng-ripple',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./ripple.component.scss']
 })
 export class RippleComponent {
+  @ViewChild('manualRipple', { static: true, read: MatRipple }) ripple?: MatRipple;
+
+  constructor(
+    @Inject(MAT_RIPPLE_GLOBAL_OPTIONS) private globalOption: RippleGlobalOptions 
+  ){}
+
+  launchRipple(){
+    const rippleRef = this.ripple?.launch({
+      persistent: true,
+      centered: true
+    });
+    // const rippleRef = this.ripple?.launch(this.globalOption);
+
+    setTimeout(()=>rippleRef?.fadeOut(), 300);
+  }
 
 }
